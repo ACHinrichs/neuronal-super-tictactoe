d35352c40066251d66446a48794ce414a010a72f
Georg Dorndorf <georg.dorndorf@rwth-aachen.de>
2018-01-24 17:33:25 +0100

    Do some things
    Signed-off-by: Georg Dorndorf <georg.dorndorf@rwth-aachen.de>

--- lnn.py
+++ lnn.py
@@ -9,11 +9,11 @@ class ConmputerPlayer(Player):
         self.number = aNumber
 
     def won(self):
-        pass
-        #lnn.updateNet()
+        #pass
+        lnn.updateNet(0)
 
     def lost(self):
-        lnn.updateNet()
+        lnn.updateNet(1)
 
     def move(self, board):
         move = lnn.move();
@@ -21,8 +21,8 @@ class ConmputerPlayer(Player):
 
 class LayerNeuralNetwork():
     move_count = 0
+    moves = []
     def nonlin(x, deriv=False):
-        moves
         if(deriv==True):
             return x*(1-x)
         return 1/(1+np.exp(-x))
@@ -33,38 +33,69 @@ class LayerNeuralNetwork():
         p2 = ComputerPlayer(2)
         
         for i in range(100):
+            print(i)
             game = Game(p1,p2)
             
     def move(self, board):
-        m = nonlin(np.dot(board, syn0))
-        x1 = m.index(max(m))
-        m[x1] = 0
-        x2 = m.index(max(m))
+        # Network makes a
+        # l0 = board
+        l1 = nonlin(np.dot(board, syn0))
+        l2 = nonlin(np.dot(l1, syn1))
+        l3 = nonlin(np.dot(l2, syn2))
+        l4 = nonlin(np.dot(l3, syn3))
+        
+        x = m.index(max(l4))
+        
+        x1 = x // 9
+        x2 = x % 9
         moves.append((x1,x2, board))
         move_count += 1
-        return [x1, x2]
+        return Coordinate(x1, x2)
 
-    def updateNet(self):
+    def updateNet(self, lost):
+
+
         move = 0
         for x,y,board in moves:
-            move += 1;
-            # how much did we miss?
+            # forward propagation
+            l0 = board
+            l1 = nonlin(np.dot(l0, syn0))
+            l2 = nonlin(np.dot(l1, syn1))
+            l3 = nonlin(np.dot(l2, syn2))
+            l4 = nonlin(np.dot(l3, syn3))
             
-            impact = move_count - move
+            # how much did we miss?
+            l4_error = lost - l4
+            
             # multiply how much we missed by the 
             # slope of the sigmoid at the values in l1
-            l1_delta = impact * nonlin(l1,True)
+            l4_delta = l4_error * nonlin(l1,True)
+
+            l3_error = l4_delta.dot(syn3.T)
+            l3_delta = l3_error * nonlin(l3, deriv=True)
+
+            l2_error = l3_delta.dot(syn2.T)
+            l2_delta = l2_error * nonlin(l2, deriv=True)
+
+            l1_error = l2_delta.dot(syn1.T)
+            l1_delta = l1_error * nonlin(l1, deriv=True)
             
             # update weights
-            syn0 += np.dot(board.T,l1_delta)
-
+            syn3 += np.dot(l4.T,l4_delta)
+            syn2 += np.dot(l3.T,l3_delta)
+            syn1 += np.dot(l2.T,l2_delta)
+            syn0 += np.dot(l1.T,l1_delta)
+            
             
 # seed random numbers to make calculation
 # deterministic
 np.random.seed(1)
 
 # initialize weights randomly with mean 0
-syn0 = 2*np.random.random((81,1)) - 1
+syn0 = 2*np.random.random((9,9)) - 1
+syn1 = 2*np.random.random((81,81)) -1
+syn2 = 2*np.random.random((81,9)) -1
+syn3 = 2*np.random.random((81,1))-1
 print(syn0)
 
 lnn = LayerNeuralNetwork()
@@ -72,5 +103,5 @@ lnn.trainNetwork()
 
 print ("Neuronales Netz 1: ")
 print (syn0)
-print (nonlin(np.dot([1,1,0], syn0)))
+#print (nonlin(np.dot([1,1,0], syn0)))
 
