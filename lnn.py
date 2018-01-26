b059f2910be8d8d1397508a59779cadd52ffa202
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2018-01-26 14:19:35 +0100

    Try to fix

--- lnn.py
+++ lnn.py
@@ -4,6 +4,12 @@
 import numpy as np
 import supertictactoe as st
 from supertictactoe import Player, Game, Coordinate, Board
+LOG_LEVEL = 0
+
+
+def log(out, level=1):
+    if level <= LOG_LEVEL:
+        print(out)
 
 class ComputerPlayer(Player):
     def __init__(self, aNumber, network):
@@ -29,22 +35,33 @@ class LayerNeuralNetwork():
 
     def __init__(self):
         # initialize weights randomly with mean 0
-        self.syn0 = 2*np.random.random((9,81)) - 1
-        self.syn1 = 2*np.random.random((81,81)) -1
-        self.syn2 = 2*np.random.random((81,81))-1
-        self.syn3 = 2*np.random.random((81,1)) -1
+        self.syn0 = 2*np.random.random((81,90)) - 1
+        self.syn1 = 2*np.random.random((81*9,81)) -1
+        self.syn2 = 2*np.random.random((81*9,81*9))-1
+        self.syn3 = 2*np.random.random((81,81*9)) -1
 
         pass
 
     def boardToInput(self, board, playerNumber):
         out = board.field[:] #Copy List!
         out.append([0,0,0,0,0,0,0,0,0])
-        outArray = Board.playerToEntry(playerNumber) * np.array(out)
-        if(not board.lastMove.bottomLevel == -1):
-            outArray[9,board.lastMove.bottomLevel]=1
+        outArray =np.zeros(90)
+        playerSymb = Board.playerToEntry(playerNumber)
+        x=0
+        for row in out:
+            y=0
+            for entry in row:
+                if(x<9):
+                    outArray[x*9+y]=entry*playerSymb
+                else:
+                    outArray[x*9+y]=entry
+                y=y+1
+            x=x+1
         return outArray
+        
     
     def nonlin(self, x, deriv=False):
+        #return np.arctan(x)
         if(deriv==True):
             return x*(1-x)
         return 1/(1+np.exp(-x))
@@ -53,12 +70,17 @@ class LayerNeuralNetwork():
         # Network makes a
         # l0 = board
         # board = board.field
-        l1 = self.nonlin(np.dot(self.boardToInput(board, playerNumber), self.syn0))
-        l2 = self.nonlin(np.dot(l1, self.syn1))
-        l3 = self.nonlin(np.dot(l2, self.syn2))
-        l4 = self.nonlin(np.dot(l3, self.syn3))
-        
-        x = l4.tolist().index(max(l4))
+        log("Move")
+        l1 = self.nonlin(np.dot(self.syn0,self.boardToInput(board, playerNumber)))
+        log(l1.shape)
+        l2 = self.nonlin(np.dot(self.syn1, l1))
+        log(l2.shape)
+        l3 = self.nonlin(np.dot(self.syn2, l2))
+        log(l3.shape)
+        l4 = self.nonlin(np.dot(self.syn3, l3))
+        log(l4.shape)
+
+        x = l4.tolist().index(max(l4.tolist()))
         x1 = x // 9
         x2 = x % 9
         return Coordinate(x1, x2)
@@ -69,35 +91,43 @@ class LayerNeuralNetwork():
             i=i+1
             # forward propagation (has to be recalculated, because we already adapted the network!
             coord = self.move(board,playerNumber)
+            log("updateNet")
 
             penality = (lost*2 - 1) * (i / len(moves)) # Is 1 if move is losing and -1 if move is winning move
             
             #Propagate Backwards to evaluate how much I have to adapt the network
-            l4 = np.zeros((1,81))
-            l4[0][coord.topLevel*9+coord.bottomLevel]=1
-            l4_delta = penality * l4 * 0.5
-            
-            
-            l3 = self.nonlin(np.dot(l4,np.linalg.inv(self.syn3)))
-            print(l3.shape)
-            l3_delta = penality * l3 * 0.25
 
-            l2 = self.nonlin(np.dot(l3,self.syn2))
+            l4 = np.zeros(81)
+            l4[coord.topLevel*9+coord.bottomLevel]=1
+            l4_delta = penality * l4 * 0.5
+
+            log(l4.shape)
+            #print(self.syn3.shape)
+            l3 = self.nonlin(np.dot(l4.T, self.syn3))
+            l3_delta = penality * l3 * 0.25
+            
+            log(l3.shape)
+            l2 = self.nonlin(np.dot(l3.T, self.syn2))
             l2_delta = penality * l2 * 0.125
             
-            l1 = self.nonlin(np.dot(l2,self.syn1))
+            log(l2.shape)
+            l1 = self.nonlin(np.dot(l2.T, self.syn1))
             l1_delta = penality * l1 * 0.0625
-            print(l3)
-            
-            #l0 = self.nonlin(np.dot(self.syn0,l1))
-            #l0_delta = penality * l0 * 0.125
             #print(l3)
             
+            log(l1.shape)
+            l0 = self.nonlin(np.dot(l1.T,self.syn0))
+            l0_delta = penality * l0 * 0.125
+            #print(l3)
+            
+            log(l0.shape)
             # update weights
-            self.syn3 += l3_delta
-            self.syn2 += l3_delta
-            self.syn1 += l2_delta
-            self.syn0 += l1_delta
+            self.syn3 = self.nonlin(self.syn3 - self.nonlin(np.dot(l3, l3_delta)))
+            self.syn2 =self.nonlin(self.syn2 - self.nonlin(np.dot(l2, l2_delta)))
+            self.syn1 = self.nonlin(self.syn1 - self.nonlin(np.dot(l1, l1_delta)))
+            #print(self.syn0.shape)
+            #print(l1_delta.shape)
+            self.syn0 = self.nonlin(self.syn0 - self.nonlin(np.dot(l0, l0_delta)))
             #self.syn3 += np.dot(l3.T,l3_delta)
             #self.syn2 += np.dot(l2.T,l3_delta)
             #self.syn1 += np.dot(l1.T,l2_delta)
@@ -123,7 +153,7 @@ lnn = LayerNeuralNetwork()
 
 # train Network
 
-for i in range(10000):
+for i in range(1000):
     p1 = ComputerPlayer(0,lnn)
     p2 = ComputerPlayer(1,lnn)
     game = Game(p1,p2)
